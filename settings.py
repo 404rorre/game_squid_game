b88e5e82f7f2095ddf69a3e458c017d572fdb26c
404rorre <404rorre.edoc@gmail.com>
2022-05-11 13:36:35 +0200

    Initializing settings for the game.

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings():
+	"""A class to store all game settings."""
+	def __init__(self):
+		"""Initializing game settings."""
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (64, 224, 208)
