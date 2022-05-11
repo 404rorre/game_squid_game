bb017927e9bbcb6ef9151127058bcfeb1f70a917
404rorre <404rorre.edoc@gmail.com>
2022-05-11 14:49:48 +0200

    correct drawing character on screen

--- character.py
+++ character.py
@@ -7,11 +7,11 @@ class Squid:
 		self.screen = game.screen
 		self.screen_rect = self.screen.get_rect()
 		#load image of game character
-		self.image = pygame.image.load("/images/squid_game.bmp")
+		self.image = pygame.image.load("images/squid_game.bmp")
 		self.rect = self.image.get_rect()
 		#Start the game character at the midleft of the screen
 		self.rect.midleft = self.screen_rect.midleft
 
 	def blitme(self):
 		"""Draw character on screen."""
-		self.image.blit(self.image, self.rect)
+		self.screen.blit(self.image, self.rect)
